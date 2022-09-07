940b76cf1ae55d7bcec30ee74bb6a1dbc6671cca
brunocs90@gmail.com <brunocs90@gmail.com>
2022-09-07 16:48:53 -0300

    estrutura da aplicação

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Button } from './components/Button'
+
 export function App() {
   return (
-    <div>Hello World</div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Button variant='primary' />
+      <Button variant='secondary' />
+      <Button variant='sucess' />
+      <Button variant='danger' />
+    </ThemeProvider>
   )
 }
