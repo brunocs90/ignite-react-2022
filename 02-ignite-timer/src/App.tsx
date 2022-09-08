5c4d7fc881c0728a973f0cee457fa88fcfd354a3
brunocs90@gmail.com <brunocs90@gmail.com>
2022-09-08 09:00:54 -0300

    pagina home

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,16 +1,17 @@
 import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router';
+
 import { GlobalStyle } from './styles/global';
-import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
     return (
         <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
             <GlobalStyle />
-            <Button variant="primary" />
-            <Button variant="secondary" />
-            <Button variant="sucess" />
-            <Button variant="danger" />
         </ThemeProvider>
     );
 }
