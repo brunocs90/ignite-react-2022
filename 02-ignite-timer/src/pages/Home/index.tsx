2e2f60d9f203dd6d4d0dc963da4e4f93f069349a
brunocs90@gmail.com <brunocs90@gmail.com>
2022-09-08 09:48:30 -0300

    aprimorando os inputs

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -9,15 +9,25 @@ export function Home() {
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         id="task"
-                        type="text"
+                        list="task-suggestions"
                         placeholder="Dê um nome para o seu projeto"
                     />
+
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Projeto 4" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesAmountInput
                         id="minutesAmount"
                         type="number"
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutos.</span>
