cc339cb931d6b127e6961a040b62cbda1f3927be
brunocs90@gmail.com <brunocs90@gmail.com>
2022-11-22 22:33:45 -0300

    indentation

--- 03-dt-money/src/styles/global.ts
+++ 03-dt-money/src/styles/global.ts
@@ -5,17 +5,21 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-  :focus {
-    outline: 0;
-    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']}
-  }
-  body {
-    background: ${({ theme }) => theme['gray-800']};
-    color: ${({ theme }) => theme['gray-100']};
-    -webkit-font-smoothing: antialiased;
-  }
-  body, input, textarea, button {
-    font: 400 1rem Roboto, sans-serif;
-  }
+}
+
+:focus {
+  outline: 0;
+  box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']}
+}
+
+body {
+  background: ${({ theme }) => theme['gray-800']};
+  color: ${({ theme }) => theme['gray-100']};
+  -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+  font: 400 1rem Roboto, sans-serif;
+}
+
 `
